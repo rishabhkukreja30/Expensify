a70e5da220356c7a7901cd7ce6ba1ef0e672544d
rishabh kukreja <rishabhkukreja30@gmail.com>
2021-06-16 18:39:26 +0530

    implemented details component

---
+++ src/components/Details/Details.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Card, CardHeader, CardContent, Typography } from '@material-ui/core'
+import {Doughnut} from 'react-chartjs-2'
+import useStyles from './styles.js'
+
+
+const Details = ({title}) => {
+const classes = useStyles()
+
+    return (
+        <Card className={title === 'Income' ? classes.income : classes.expense}>
+            <CardHeader title={title}/>
+            <CardContent>
+                <Typography variant="h5">₹500</Typography>
+                {/* <Doughnut data ="Data" /> */}
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Details
