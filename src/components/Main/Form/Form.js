62e4e580c10e90f4673ecdc1555d059ac1b3987e
rishabh kukreja <rishabhkukreja30@gmail.com>
2021-06-17 15:16:52 +0530

    adding transactions without clicking create

--- src/components/Main/Form/Form.js
+++ src/components/Main/Form/Form.js
@@ -1,4 +1,4 @@
-import React , {useState, useContext} from 'react'
+import React , {useState, useContext, useEffect} from 'react'
 import {TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem} from '@material-ui/core'
 import { ExpenseTrackerContext } from '../../../context/context'
 import {v4 as uuidv4} from 'uuid'
@@ -24,6 +24,8 @@ const Form = () => {
     const {segment} = useSpeechContext()
 
     const createTransaction = () => {
+        if(Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return
+
         const transaction = {...formData, 
             amount : Number(formData.amount), 
             id: uuidv4()
@@ -33,6 +35,53 @@ const Form = () => {
 
         setFormData(initialState)
     }
+
+    useEffect(() => {
+        if(segment) {
+            if(segment.intent.intent === 'add_expense') {
+                setFormData({...formData, type: 'Expense'})
+            } 
+            else if(segment.intent.intent === 'add_income') {
+                setFormData({...formData, type: 'Income'})
+            }
+            else if(segment.isFinal && segment.intent.intent === "create_transaction") {
+                return createTransaction()
+            }
+            else if(segment.isFinal && segment.intent.intent === "cancel_transaction") {
+                return setFormData(initialState)
+            }
+
+            segment.entities.forEach((e) => {
+                    const category = `${e.value.charAt(0)}${e.value.slice(1).toLowerCase()}`
+
+                    switch(e.type) {
+                        case 'amount':
+                            setFormData({...formData, amount: e.value})
+                            break
+
+                        case 'category':
+                            if(incomeCategories.map((iC) => iC.type).includes(category)) {
+                                setFormData({...formData, type: 'Income',  category})
+                            }
+                            else if(expenseCategories.map((iC) => iC.type).includes(category)) {
+                                setFormData({...formData, type: 'Expense',  category})
+                            }
+                            break
+
+                            
+                        case 'date':
+                            setFormData({...formData, date: e.value})
+                            break
+                        default:
+                            break
+                    }
+            })
+
+            if(segment.isFinal && formData.amount && formData.category && formData.type && formData.date) {
+                createTransaction()
+            }
+        }
+    }, [segment])
 
     const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories
 
