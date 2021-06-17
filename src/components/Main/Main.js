18b463f1c9b3a2ab6231a5abf8b7a2a6e1675544
rishabh kukreja <rishabhkukreja30@gmail.com>
2021-06-17 15:53:30 +0530

    added infocard and dynamic balance

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React , {useContext} from 'react'
 import {Card, CardHeader, CardContent, Typography , Grid, Divider} from '@material-ui/core'
 import Form from './Form/Form'
 import List from './List/List'
+import {ExpenseTrackerContext} from '../../context/context'
+import InfoCard from '../InfoCard'
 import useStyles from './styles'
 
 
 const Main = () => {
-const classes = useStyles()
+    const classes = useStyles()
+
+    const {balance} = useContext(ExpenseTrackerContext)
 
     return (
         <div>
             <Card className={classes.root}>
                 <CardHeader title="Expense Tracker" subheader="Powered by Speechly"/>
                 <CardContent>
-                    <Typography align="center" variant="h5">Total Balance ₹ 10000</Typography>
+                    <Typography align="center" variant="h5">Total Balance ₹ {balance}</Typography>
                     <Typography variant="subtitle1" style={{lineHeight: '1.5em', marginTop: '20px'}}>
-                         {/* InfoCard */}
-                         Try Saying : Add income for ₹ 2000 in category salary for Monday
+                        < InfoCard />
+
                     </Typography>
                     <Divider />
                     <Form />
